1c2cba224131e1f5c0bb48c4f8e6d5c2d1b44f41
Tiratom <217bssr@gmail.com>
2018-04-08 11:56:16 +0900

    試合中でない場合の処理作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   include OrdersHelper
   def index
     player_name = '川端慎吾'
-    playing_member = get_playing_member
-    
+    @playing_info = get_playing_info(player_name)
   end
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,32 +4,33 @@ require 'selenium-webdriver'
 
 module OrdersHelper
 
-  def get_playing_member
+  def get_playing_info(player_name)
     driver = Selenium::WebDriver.for :firefox
 
     driver.navigate.to "https://www.yakult-swallows.co.jp"
-    byebug
     game_info_link = driver.find_element(:class, "index-game-live")
-    if game_info_link.nil?
-      return 'No Game'
-    end
     game_info_link.click
     game_info_page_details = driver.find_elements(:class, "wrap")
     game_info_page_details[1].find_elements(:tag_name, "a")[1].click
     playing_member_by_team = driver.find_elements(:class, "list-plalyers-game")
+
     playing_meber_by_team.each do |team, index|
       if team[index].find_elemnt(:taga_name, "h3").text ==  "ヤクルト 出場中選手"
+        playing_members = driver.find_elements(:class, "list-players-game")[index].find_elements(:tag_name, "h2")
       end
     end
 
-    playing_members = driver.find_elements(:class, "list-players-game")[index].find_elements(:tag_name, "h2")
     playing_member.each do |member|
-      if member.text == "kawabata"
+      if member.text == player_name
+        return '試合にでています！'
       end
     end
+    return '試合には出ていないようです・・・。'
 
-    return True
-
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    return '試合中ではないようです。'
+  rescue
+    return 'データ取得中に何らかのエラーが発生しました。'
   end
 
 end
